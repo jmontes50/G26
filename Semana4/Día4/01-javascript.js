656c114aa5f01938c572918963e73a64504a1fa1
Osmar Montesinos <osmarmontes50@gmail.com>
2024-07-25 22:15:29 -0500

    avance

---
+++ Semana4/Día4/01-javascript.js
@@ -0,0 +1,29 @@
+console.log("Hola Mundo desde JS!!!");
+//Estamos conociendo el console.log
+//Este puede recibir varios argumentos
+//y si quiero... etc.
+/**
+ * Podemos escribir
+ * en diferentes
+ * lineas2
+ */
+//puede recibir varios argumentos
+console.log("Hola", "este es otro texto", 10);
+console.log("revisando variable:", 20);
+
+//Tipos de datos
+//tipado dinámico - el tipo de dato se va a definir con el valor asignado
+//declarar y asignar
+let nombre = "Dennis";
+
+let apellido; //declarando
+apellido = "Perez"; //asignando
+
+console.log(nombre);
+
+let edad = 25;
+edad = 30;
+console.log("1.", edad);
+
+edad = "treinta y uno";
+console.log("2.", edad)

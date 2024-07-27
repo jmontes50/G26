8c99fa3529c0d663173cf7f1f5dcfc71ded21044
Osmar Montesinos <osmarmontes50@gmail.com>
2024-07-26 22:05:10 -0500

    avance

---
+++ Semana4/Día5/03-condicionales.js
@@ -0,0 +1,23 @@
+//expresión
+// algo que me da un valor
+
+//palabra reservada - que ya esta reservado con un significado para otra cosa y que no se puede utilizar
+/**
+ * if (condicional/expresion){
+ *  //si la expresión es verdadera se ejecutará el codigo despues de las llaves del if
+ * }else {
+ * //se ejecutrará el codigo dentro de las llaves de else
+ * }
+ * //if -> si
+ * //else -> esto otro
+ */
+
+let nota = 15;
+
+if(nota > 12){
+    //si la expresión es V
+    console.log("Esta aprobado(a)");
+}else{
+    //si la expresión es F esto otro
+    console.log("Desaprobo");
+}

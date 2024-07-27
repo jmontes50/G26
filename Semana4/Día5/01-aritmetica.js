c7c53cef285462609c487cdc80fe1a14543b5add
Osmar Montesinos <osmarmontes50@gmail.com>
2024-07-26 19:39:58 -0500

    avance

---
+++ Semana4/Día5/01-aritmetica.js
@@ -0,0 +1,11 @@
+// operadores aritméticos
+let cpu = 300;
+let memoria = 200;
+let monitor = 500;
+
+//suma, vamos a utilizar el operador +
+let precioTotal = cpu + memoria + monitor;
+console.log("El precio total es:", precioTotal);
+
+let accesorios = 50 + 90;
+console.log("accesorios:", accesorios);

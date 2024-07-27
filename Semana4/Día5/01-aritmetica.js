8ea01ac2fbe6e78b7c4829f28cad88cd8dee2477
Osmar Montesinos <osmarmontes50@gmail.com>
2024-07-26 19:45:05 -0500

    avance

--- Semana4/Día5/01-aritmetica.js
+++ Semana4/Día5/01-aritmetica.js
@@ -9,3 +9,15 @@ console.log("El precio total es:", precioTotal);
 
 let accesorios = 50 + 90;
 console.log("accesorios:", accesorios);
+
+//restar
+let cupon = 25;
+let precioConDescuento = precioTotal - cupon;
+console.log("Con descuento:", precioConDescuento);
+
+//multiplicar
+let totalPorDos = precioConDescuento * 2;
+console.log("multiplicación por 2", totalPorDos);
+
+let conIGV = totalPorDos * 1.17;
+console.log("con IGV", conIGV);

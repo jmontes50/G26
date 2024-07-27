0ebc668e452855554c6e2fefc9eaec81ae28f439
Osmar Montesinos <osmarmontes50@gmail.com>
2024-07-26 20:46:22 -0500

    avance

--- Semana4/Día5/02-concatenacion.js
+++ Semana4/Día5/02-concatenacion.js
@@ -3,3 +3,20 @@ let nombre = "Juan";
 let saludoCompleto = saludo + " " + nombre + ' Perez';
 
 console.log(saludoCompleto);
+
+let edad = 30;
+let meses = "8";
+
+console.log(edad + meses);
+console.log(edad - meses);
+
+//template string --> ${}
+let despedida = "Hasta Luego";
+
+let despedidaCompleta = `
+Nos vemos ${nombre}, ${despedida}.
+Hasta la siguiente buen amigo
+Adiós! 
+Mientras tanto ${ 1 + 1 } más!
+`;
+console.log(despedidaCompleta);

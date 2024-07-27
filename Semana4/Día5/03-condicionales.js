e07a78f1963616dbfb7f864220cdb6a8695b4b03
Osmar Montesinos <osmarmontes50@gmail.com>
2024-07-26 22:14:21 -0500

    avance

--- Semana4/Día5/03-condicionales.js
+++ Semana4/Día5/03-condicionales.js
@@ -14,10 +14,26 @@
 
 let nota = 15;
 
-if(nota > 12){
+// if(nota > 12){
+if(nota >= 13){
     //si la expresión es V
     console.log("Esta aprobado(a)");
 }else{
     //si la expresión es F esto otro
     console.log("Desaprobo");
+}
+
+let edad = 20;
+// con == estamos evaluando el valor, pero no el tipo de dato, no es recomendable
+/*if(edad == "20"){
+    console.log("es igual que 20");
+}else {
+    console.log("NO es igual que 20");
+}*/
+
+// con === estamos evaluando el valor y el tipo de dato, es lo que deberiamos utilizar siempre
+if(edad === "20"){
+    console.log("es igual que 20");
+}else {
+    console.log("NO es igual que 20");
 }

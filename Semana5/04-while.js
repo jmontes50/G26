d9d33865a96633ab287479f59ddcd6f51113efad
Jorge Montesinos <osmarmontes50@gmail.com>
2024-07-30 23:01:25 -0500

    avance

--- Semana5/04-while.js
+++ Semana5/04-while.js
@@ -7,10 +7,19 @@
 
 let contador = 0;
 
-while(contador <= 10) {
+while(contador <= 3) {
     console.log(`Contandoooo! ${contador}`);
     //contador = contador + 1;
     contador++;
 }
 //cuando ya termino de ejecutarse el bucle, recién continua con el código que continue después al cerrar las llaves.
 console.log("Fin!!!");
+
+//------------------------------
+let logueado = true;
+
+while(logueado === true) {
+  // yo podria realizar alguna tarea, siempre y cuando este logueado
+  console.log("Trabajando... ");
+  logueado = confirm("Desea mantener su sesión?");
+}

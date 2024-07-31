f451f4673efd300ef0305a090aaf964904f00c2c
Jorge Montesinos <osmarmontes50@gmail.com>
2024-07-30 20:52:39 -0500

    avance

---
+++ Semana5/02-operadoreslogicos.js
@@ -0,0 +1,19 @@
+//Deadpool & Wolverine, es una pelicula para mayores de 13 años, pero si eres menor de 18 tienes que ir con tus padres
+//tienes que presentar identificación
+//Que variables, cosas tenemos claras
+/**
+ * 1, necesito saber la edad
+ * 2, necesito la identificación
+ * 3, si es menor de 18 pero mayor que 13 puede entrar con sus padres
+ */
+//colocando el símbolo + delante del prompt lo convierte a número
+let edad = +prompt("Cúal es tu edad");
+let traeIdentificacion = confirm("Traes identificación?");
+// console.log("trae identificación", traeIdentificacion);
+let vieneConApoderado = confirm("Vienes con algún apoderado?");
+
+if(edad >= 18 && traeIdentificacion === true) {
+    console.log("Puede ver la película");
+}else if(edad < 18 && edad > 13 && vieneConApoderado === true) {
+    console.log("Puede ver la película");
+}

b8da1025f29aa9004db788b321ca5c2797f72625
Jorge Montesinos <osmarmontes50@gmail.com>
2024-07-30 20:14:47 -0500

    avance

--- Semana5/01-elseif.js
+++ Semana5/01-elseif.js
@@ -6,18 +6,26 @@ console.log(typeof hora);
 let numHora = parseFloat(hora);
 console.log("numhora", typeof numHora);
 //NaN -> Not A Number, su tipo es númerico
-console.log(isNaN(numHora))
+//isNaN me da un booleano para indicar si algo es Not a Number, si es NaN me dara true
+console.log(isNaN(numHora));
 
-if (numHora < 12) {
-  console.log("Buenos días");
-  //para poner más condiciones, utilizamos la sentencia else if
-}else if (numHora < 18) {
-  console.log("Buenas tardes");
-  //otra condición
-}else if (numHora <= 24){
-  console.log("Buenas noches");
-  //y en caso no se cumpla ninguna siempre quedara el else por defecto/default
-}else {
-  console.error("Ingrese un valor correcto");
+let noEsNumero = isNaN(numHora);
+
+if (noEsNumero === true) {
+  console.log("No has ingresado un número");
+} else {
+  //pero si, si es un numero
+  //ya hago el resto del trabajo asegurandome que es un número.
+  if (numHora < 12) {
+    console.log("Buenos días");
+    //para poner más condiciones, utilizamos la sentencia else if
+  } else if (numHora < 18) {
+    console.log("Buenas tardes");
+    //otra condición
+  } else if (numHora <= 24) {
+    console.log("Buenas noches");
+    //y en caso no se cumpla ninguna siempre quedara el else por defecto/default
+  } else {
+    console.error("Ingrese un valor correcto");
+  }
 }
-

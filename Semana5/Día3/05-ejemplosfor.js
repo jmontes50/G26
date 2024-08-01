7a97f61a39ad996d2e39cb28a0a9901ab4eb3689
Jorge Montesinos <osmarmontes50@gmail.com>
2024-07-31 21:51:40 -0500

    avance

--- Semana5/Día3/05-ejemplosfor.js
+++ Semana5/Día3/05-ejemplosfor.js
@@ -2,6 +2,7 @@
  * Simulación de ahorro mensual
  */
 
+/*
 let ahorroMensual = 200;
 let meses = 12;
 let totalAhorrado = 0;
@@ -17,3 +18,20 @@ for(let mes = 1; mes <= meses; mes++){
 }
 
 console.log(`En ${meses} meses se ahorro un total de: ${totalAhorrado}`);
+
+
+/**
+ * Calcular el interés compuesto: Una caja de ahorros nos brinda un interés compuesto del 3% por cada mes, calcular el interés del monto ahorrado en un lapso de 6 meses
+ */
+
+let montoBase = 2000;
+let tasa = 0.03; // 3 / 100;
+let meses = 6;
+
+for(let i = 1; i <= meses; i++){
+  // montoBase = montoBase * (1 + tasa);
+  montoBase = montoBase + (montoBase * tasa);
+  console.log(`En el mes ${i} el monto creció a: ${montoBase}`);
+}
+//toFixed retorna un string
+console.log(`El monto después de los ${meses} meses creció a: ${montoBase.toFixed(2)}`);

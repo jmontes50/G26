6210bbe16836d7053518ef027f1d2e6134f091ca
Jorge Montesinos <osmarmontes50@gmail.com>
2024-07-31 20:26:10 -0500

    avance

---
+++ Semana5/Día3/03-do-while.js
@@ -0,0 +1,15 @@
+// let contador = 0;
+let contador = 5;
+
+while(contador < 4) { //si la condicional no se cumple, no ejecuta nada
+    console.log(contador);
+    contador++;
+}
+
+
+do {
+    //el código a ejecutar
+    console.log(contador);
+    contador++;
+    //while(expresión a evaluar)
+}while(contador < 10);

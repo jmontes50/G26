086312c2233483b0277d13a0bd1f0c5917347077
Jorge Montesinos <osmarmontes50@gmail.com>
2024-07-31 22:05:35 -0500

    avance

--- Semana5/Día3/05-ejemplosfor.js
+++ Semana5/Día3/05-ejemplosfor.js
@@ -24,6 +24,7 @@ console.log(`En ${meses} meses se ahorro un total de: ${totalAhorrado}`);
  * Calcular el interés compuesto: Una caja de ahorros nos brinda un interés compuesto del 3% por cada mes, calcular el interés del monto ahorrado en un lapso de 6 meses
  */
 
+/*
 let montoBase = 2000;
 let tasa = 0.03; // 3 / 100;
 let meses = 6;
@@ -33,5 +34,31 @@ for(let i = 1; i <= meses; i++){
   montoBase = montoBase + (montoBase * tasa);
   console.log(`En el mes ${i} el monto creció a: ${montoBase}`);
 }
-//toFixed retorna un string
+//toFixed retorna un string con los decimales limitados al número que reciba
 console.log(`El monto después de los ${meses} meses creció a: ${montoBase.toFixed(2)}`);
+*/
+
+/**
+ * Una clínica da un descuento a sus pacientes según el número de citas/consultas que tomen con la clínica.
+ * 
+ * a. las primeras 4 citas costarán 100 soles
+ * b. las 5 siguientes costarán 80 soles
+ * c. y las demás después de las indicadas costarán 75 c/u
+ * 
+ * Se requiere saber en base a la cantidad de consultas cuanto tiene que pagar el paciente al finalizar su tratamiento.
+ */
+
+let cantConsultas = 6;
+let costoTotal = 0;
+
+for(let i = 1; i <= cantConsultas; i++){
+  if( i <= 4 ){ //a.
+    costoTotal += 100;
+  } else if ( i <= 9 ){ // b.
+    costoTotal += 80;
+  } else { //c.
+    costoTotal += 75;
+  }
+}
+
+console.log(`El costo total a pagar es de: S/ ${costoTotal}`);

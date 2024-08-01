1080ad3512ce37dc9ef878ba3b49f83f072f8016
Jorge Montesinos <osmarmontes50@gmail.com>
2024-07-31 22:42:21 -0500

    avance

---
+++ Semana5/Día3/06-arrays.js
@@ -0,0 +1,16 @@
+let estudiante1 = "Fernando";
+let estudiante2 = "Herber";
+let estudiante3 = "Dennis";
+
+//para definir un arreglo abrimos corchetes []
+let arrEstudiante = ["Fernando", "Herber", "Dennis", "Alicia"];
+
+console.log(arrEstudiante);
+//si deseamos acceder a 01 solo item dentro del arreglo habra que llamar al nombre del arreglo e indicar el indice en corchetes
+console.log(arrEstudiante[2]);
+
+console.log(arrEstudiante[0]);
+
+//si no se acuerdan coo escribir length, Gallina Tiene Huevos
+console.log("Longitud:", arrEstudiante.length);
+console.log("Type Length:", typeof arrEstudiante.length);

94219b24c28af97a240d3b1cf63205477f9e5405
Jorge Montesinos <osmarmontes50@gmail.com>
2024-08-01 20:17:31 -0500

    avance

--- Semana5/Día4/02-ejemplos-arr-for.js
+++ Semana5/Día4/02-ejemplos-arr-for.js
@@ -2,6 +2,7 @@
  * Tendremos un array/arreglo de números enteros positivos, donde se buscará modificar el arreglo para que si es un número impar este se multiplique por 2, esto debe aplicarse a todos los números impares encontrados
  */
 
+/*
 let numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
 for(let i = 0; i < numeros.length; i++) {
@@ -13,3 +14,24 @@ for(let i = 0; i < numeros.length; i++) {
 }
 
 console.log(numeros);
+*/
+
+/**
+ * a partir de un número inicial y un número final que sea mayor que el inicial, genere un arreglo de números enteros consecutivos en el rango de esos dos números
+ */
+
+let numInicial = 13;
+let numFinal = 20;
+//[13,14,15,16,...,20]
+let rango = []; //""
+// console.log(rango.length);
+
+for(let i = numInicial; i <= numFinal; i++){
+    //push agregaba 01 item en la última posición del array, siempre al final
+    rango.push(i);
+    // console.log(rango)
+}
+
+console.log(rango);
+
+

127283a7535d08da6d545c6b0c78f9159fe5a4b9
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-03 10:08:52 -0500

    avance

--- Semana5/Día6/01-ejercicios-objetos.js
+++ Semana5/Día6/01-ejercicios-objetos.js
@@ -1,3 +1,4 @@
+/*
 let productos = [
   {
     nombre: "Chocolates",
@@ -32,3 +33,64 @@ let resultado = filtrarPorPrecio(productos, 100);
 
 console.log(`Los productos a partir de 100 son`);
 console.table(resultado);
+*/
+
+/**
+ * Se tiene un arreglo de alumnos (objetos), donde cada alumno tiene su nombre y notas acumuladas del curso, el objetivo es dar un resumen donde se indique el nombre del alumno y su promedio y que indique si esta aprobado(a) (mayor ó igual de 13)
+ */
+
+let alumnos = [
+  {
+    nombre: "Juan",
+    notas: [18, 14, 17]
+  },
+  {
+    nombre: "Angela",
+    notas: [19, 13, 16]
+  },
+  {
+    nombre: "Elvis",
+    notas: [15, 18, 20]
+  },
+  {
+    nombre: "David",
+    notas: [12, 11, 13]
+  },
+  {
+    nombre: "Edwin",
+    notas: [15, 12, 16]
+  }
+];
+
+let resumenNotas = (arrAlumnos) => {
+  //la idea de este 1er for es que pasemos Alumno x alumno
+  let resumen = [];
+  for (let i = 0; i < arrAlumnos.length; i++){
+    let alumnoTemporal = {
+      nombre: arrAlumnos[i].nombre,
+      promedio: 0, //valores por defecto
+      aprobado: false //valores por defecto
+    }
+    //Las notas de c/alumno estan como un array
+    let sumaNotas = 0;
+    let cantidadNotas = arrAlumnos[i].notas.length
+    for(let j = 0; j < cantidadNotas; j++){
+      sumaNotas +=  arrAlumnos[i].notas[j];
+      //terminará de sumar las notas
+    }
+    //obtenemos el promedio redondeadolo a 01 decimal y convirtiendolo a number con parseFloat
+    // console.log(sumaNotas)
+    alumnoTemporal.promedio = parseFloat((sumaNotas / cantidadNotas).toFixed(1));
+    //en base al promedio indicamos si esta aprobado o no
+    alumnoTemporal.aprobado = alumnoTemporal.promedio >= 13;
+    // if(alumnoTemporal.promedio >= 13){
+    //   alumnoTemporal.aprobado = true
+    // } else {
+    //   alumnoTemporal.aprobado = false
+    // }
+    resumen.push(alumnoTemporal)
+  }
+  return resumen;
+}
+
+console.table(resumenNotas(alumnos));

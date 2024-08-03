21011e1fcbba58a6cd2ab2df9e6b8f7461f6a2f1
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-02 20:51:10 -0500

    avance

---
+++ Semana5/Día5/02-breakcontinue.js
@@ -0,0 +1,20 @@
+let precios = [20, 50, 30, 10, 5, 5, 20, 45];
+
+let presupuesto = +prompt("Cúal es su presupuesto?");
+
+let cantAComprar = 0;
+
+let vuelto = 0;
+
+for(let i = 0; i < precios.length; i++){
+    console.log(`Observando el precio ${i}`);
+    if(precios[i] <= presupuesto){
+        console.log(`El prod. ${i} se compro!`);
+        cantAComprar++;
+        presupuesto -= precios[i];
+    }
+}
+
+vuelto = presupuesto;
+
+console.log(`Ud. compro ${cantAComprar} productos y su vuelto es: S/ ${vuelto}`);

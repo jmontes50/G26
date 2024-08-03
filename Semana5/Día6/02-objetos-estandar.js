5dcf52fd1802680a01e5f5af4924c175be0fe743
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-03 11:30:41 -0500

    avance

---
+++ Semana5/Día6/02-objetos-estandar.js
@@ -0,0 +1,20 @@
+let gato = {
+    nombre: "Cat"
+}
+
+console.log(gato.nombre);
+
+let nombre = "Juan";
+
+console.log(nombre.length);
+
+// let apellido = "Perez";
+let apellido = new String("Perez");
+console.log(apellido)
+console.log(typeof apellido)
+
+//Prototypes - prototipos
+
+let nombres = ["Carla", "Ximena", "Sol"];
+
+console.log(nombres);

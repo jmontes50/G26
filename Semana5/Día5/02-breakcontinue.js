4b09e917e2c929b79bc3fbe03e602fac61180174
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-02 21:04:05 -0500

    avance

--- Semana5/Día5/02-breakcontinue.js
+++ Semana5/Día5/02-breakcontinue.js
@@ -1,3 +1,5 @@
+//BREAK
+/*
 let precios = [20, 50, 30, 10, 5, 5, 20, 45];
 
 let presupuesto = +prompt("Cúal es su presupuesto?");
@@ -9,7 +11,7 @@ let vuelto = 0;
 for(let i = 0; i < precios.length; i++){
     console.log(`Observando el precio ${i}`);
     if(presupuesto === 0){
-        //va a cortar la ejecución de for
+        //va a cortar toda la ejecución de for
         break;
     }
 
@@ -23,3 +25,23 @@ for(let i = 0; i < precios.length; i++){
 vuelto = presupuesto;
 
 console.log(`Ud. compro ${cantAComprar} productos y su vuelto es: S/ ${vuelto}`);
+*/
+
+//CONTINUE
+let notas = [11, 13, 14, 20, 19, 12, 14, 10];
+
+//for que al final solamente me de las notas aprobatorias
+
+let aprobados = [];
+
+for(let i = 0; i < notas.length; i++){
+    if(notas[i] < 13){
+        //va a hacer que se salte 01 iteración
+        console.log("me salto");
+        continue;
+    }
+    aprobados.push(notas[i]);
+    console.log("Más trabajoooooo...");
+}
+
+console.log(aprobados);

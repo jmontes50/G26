8a6f51827c6ae2a85361a2a265a9697a376fa3d3
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-03 12:50:23 -0500

    avance

--- Semana5/Día6/04-Math.js
+++ Semana5/Día6/04-Math.js
@@ -4,7 +4,7 @@ console.log(PI);
 let numEuler = Math.E;
 console.log(numEuler);
 
-let numero = -25.4;
+let numero = -25.4657645671;
 
 console.log(Math.abs(numero)); //Absoluto
 
@@ -13,3 +13,11 @@ console.log(Math.ceil(numero)); //redonde para arriba, ceil es techo
 console.log(Math.floor(numero)); //redondearlo para abajo, floor es piso
 
 console.log(Math.round(numero)); //redondeo al numéro mas cercano
+
+console.log(numero.toFixed(2));
+
+console.log(Math.random()); //un número aleatorio entre 0 y 1
+
+let num2 = 25;
+
+console.log(Math.sqrt(num2));

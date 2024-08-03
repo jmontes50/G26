c8c9d3e0309e3f97f4cafeb9d8c880ed5890253d
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-02 21:24:11 -0500

    avance

---
+++ Semana5/Día5/03-objetos.js
@@ -0,0 +1,17 @@
+let gato = {
+    //nomPropiedad: Valor,
+    nombre: "Abby",
+    edad: 6,
+    color: "blanco",
+    esAmigable: false,
+    "peso-total": 4,
+}
+
+console.log(gato);
+
+//accediendo por notación de punto
+console.log(gato.nombre);
+
+//accediendo por notación de corchete
+console.log(gato['edad']);
+console.log(gato["peso-total"]);

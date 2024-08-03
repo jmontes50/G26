0d92242558c76e948853a76c71e8913acb83e6bc
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-03 12:46:57 -0500

    avance

---
+++ Semana5/Día6/04-Math.js
@@ -0,0 +1,15 @@
+let PI = Math.PI;
+console.log(PI);
+
+let numEuler = Math.E;
+console.log(numEuler);
+
+let numero = -25.4;
+
+console.log(Math.abs(numero)); //Absoluto
+
+console.log(Math.ceil(numero)); //redonde para arriba, ceil es techo
+
+console.log(Math.floor(numero)); //redondearlo para abajo, floor es piso
+
+console.log(Math.round(numero)); //redondeo al numéro mas cercano

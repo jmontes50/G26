914ee852518208229e9eeae1bf13ee4e91653363
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-02 22:25:10 -0500

    avance

---
+++ Semana5/Día5/04-objetoscomplejos.js
@@ -0,0 +1,23 @@
+let atletas = [
+    {
+        nombre: 'Kimberly Garcia',
+        deporte: "Marcha Atlética"
+    },
+    {
+        nombre: 'Alonso Correa',
+        deporte: 'Surf'
+    },
+    {
+        nombre:'Gladys Tejeda',
+        deporte: 'Maratón'
+    },
+    {
+        nombre:'María Luisa Doig',
+        deporte: 'Esgrima'
+    }
+];
+
+//Veamos a JS como un lenguaje con sus reglas, que mientras las cumplamos va a funcionar
+console.log(atletas);
+console.log(atletas[1]);
+console.log(atletas[1].nombre);

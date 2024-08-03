15e5ae01e6cfc7847e6c4ef75298e557d7ee1d77
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-03 12:19:00 -0500

    avance

--- Semana5/Día6/02-objetos-estandar.js
+++ Semana5/Día6/02-objetos-estandar.js
@@ -1,4 +1,4 @@
-let gato = {
+/*let gato = {
     nombre: "Cat"
 }
 
@@ -23,3 +23,30 @@ console.log(typeof apellido)
 let nombres = ["Carla", "Ximena", "Sol"];
 
 console.log(nombres);
+*/
+
+let nums = [1, 2, 3, 4];
+
+console.log(nums);
+console.log(typeof nums);
+
+//vamos a tener propiedades y métodos a disposición para facilitarnos ciertar tareas
+//y al acceder a estos méotods y propiedades se envidencia que son objetos por la sintaxis al utilizarlos Objeto.Propiedad u Objeto.Método
+nums.push(5);
+console.log(nums.length);
+
+let numero = 20;
+console.log(numero);
+//al invocar a .toString() lo esta envolviendo en un objeto temporal
+let numATexto = numero.toString(); //lo convertimos a un string
+console.log(numATexto);
+console.log(typeof numATexto);
+
+//Javascript automaticamente crea un objeto temporal cuando necesitamos hacer más operaciones y asi nos facilita el acceso
+
+let numObj = new Number(100); //forzando las cosas, para que se quede como obj
+console.log(numObj);
+console.log(typeof numObj);
+
+let suma = 50 + numObj;
+console.log(suma);

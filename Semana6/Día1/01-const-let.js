2955f76469e30474e4380b1ff5151a982792c146
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-05 20:15:59 -0500

    avance

--- Semana6/Día1/01-const-let.js
+++ Semana6/Día1/01-const-let.js
@@ -18,3 +18,14 @@ console.log(edad);
 let anio =  2024;
 
 console.log("let", anio);
+
+//CONST
+const IGV = 0.18;
+//no se puede cambiar el valor
+// IGV = 0.15;
+
+const edades = [10, 20, 30, 40];
+
+// edades = [10, 20, 30, 40, 50];
+edades.push(50);
+console.log(edades);

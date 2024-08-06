1e05124da9daca5d0ac51d66122164c0bb10281e
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-05 23:00:08 -0500

    avance

--- Semana6/Día1/06-modulo-principal.js
+++ Semana6/Día1/06-modulo-principal.js
@@ -1,13 +1,16 @@
 //Si importamos algo por defecto le podemos cambiar el nombre
-import Sumatoria from "./06-secundario.js";
+// import Sumatoria from "./06-secundario.js";
 //al importar cosas que no sean por defecto, tenemos que traerlo entre llaves {}
 //también tenemos que respetar los nombres de lo que traemos
-import { PI, medidas } from "./06-secundario.js";
+//para utilizar alias, utilizamos as ---> francisco as pancho
+// import { PI, medidas as sizes } from "./06-secundario.js";
+
+import Sumatoria, { PI, medidas as sizes } from "./06-secundario.js";
 
 const res = Sumatoria(10, 20);
 
 console.log(res);
-
-console.log({ medidas });
+// console.log({ medidas });
+console.log({ sizes });
 
 console.log({ PI });

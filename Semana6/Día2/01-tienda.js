5f0d7b1cac3f1ff313ab9f58ab35e96ebfe76174
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-06 20:04:27 -0500

    avance

--- Semana6/Día2/01-tienda.js
+++ Semana6/Día2/01-tienda.js
@@ -1,13 +1,27 @@
-//podemos importarlo y cambiarle el nombre
+//podemos importarlo y cambiarle el nombre directamente, siempre y cuando sea por default
 import misLibros from "./data.js";
 
 const biblioteca = {
   libros: [],
   construirLibros: function(arrLibros) {
     this.libros = arrLibros;
+  },
+  generarId: function() {
+    //necesito en base a los libros el id que sea mayor + 1, para el siguiente libros
+    //con map de un arr de objetos lo convertimos a un arr con numeros de solos los ids
+    const ids = this.libros.map((libro) => libro.id);
+    // console.log("ids:", ids);
+    const ultimoId = Math.max(...ids);
+    return ultimoId + 1;
+  },
+  // agregarLibro: function(libro) {
+  agregarLibro: function(libro) {
+
   }
 };
 
 biblioteca.construirLibros(misLibros);
 
 console.table(biblioteca);
+
+biblioteca.generarId();

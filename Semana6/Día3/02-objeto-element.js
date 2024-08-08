b7196ac9bf233010c7246457bd0230d6febc38ea
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-07 22:19:12 -0500

    avance

--- Semana6/Día3/02-objeto-element.js
+++ Semana6/Día3/02-objeto-element.js
@@ -3,6 +3,7 @@
 //createElement: me va a permitir crear elementos que representen elementos del HTML
 
 //document.createElement("EtiquetaHTML")
+//creamos la silla aparte, afuera en un taller
 //creando <img> que no esta dentro del dom, del html
 const imagen = document.createElement("img");
 //solamente existe dentro de JS como un objeto
@@ -15,3 +16,20 @@ const novedades = document.getElementById("novedades");
 //estamos agregando la silla a la habitación
 //estamos colocando el elemento como el último hijo de un elemento padre
 novedades.appendChild(imagen);
+
+//Modificando atributos
+imagen.setAttribute("alt", "pinguino tomando sol");
+imagen.style.width = "400px";
+
+//si intentamos obtener algo que todavia no existe, no lo vamos a encontrar, es importante tener en cuenta que se esta renderizando y si al momento de buscar algo eso existe
+const primicia = document.getElementById("primicia");
+console.log("primicia", primicia);
+
+///////////
+const olimpiadas = document.getElementById('olimpiadas');
+//no deja de ser un string
+olimpiadas.innerHTML = `
+    <p id="primicia">
+        Turco gana 2da medalla de plata en tiro
+    </p>
+`

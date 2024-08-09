934eff77f5dfbdbba96bb5251e01274358ef28f3
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-08 20:09:50 -0500

    avance

--- Semana6/Día4/03-eventos.js
+++ Semana6/Día4/03-eventos.js
@@ -6,3 +6,16 @@ btn.addEventListener("click", () => {
   //el código que le demos aquí se va a ejecutar
   alert("Hiciste click!!!!");
 });
+
+const input = document.getElementById("inputinfo");
+
+//el evento change va a detectar cuando un input, select, cambie
+//en la función que damos al addEventListener podemos dar un parametro que represente el evento
+input.addEventListener('change', (evento) => {
+  //target -> objetivo
+  console.log("evento", evento);
+  //el propio elemento desde donde esta saliendo el evento
+  console.log("target", evento.target);
+  //puedo obtener el valor del input directamente de su evento
+  console.log("value", evento.target.value);
+})

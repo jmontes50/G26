8e14fd4ee95a760ab4b2915fd86e756f41a1fe21
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-08 19:53:50 -0500

    avance

---
+++ Semana6/Día4/03-eventos.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("btn");
+
+// eventos
+//elemento.addEventListener('nombreEvento', funciónAEjecutar);
+btn.addEventListener("click", () => {
+  //el código que le demos aquí se va a ejecutar
+  alert("Hiciste click!!!!");
+});

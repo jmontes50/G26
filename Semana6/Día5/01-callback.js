3d30f05441d4b7d6be3b035c4434dec7a3740943
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-09 19:51:16 -0500

    avance

--- Semana6/Día5/01-callback.js
+++ Semana6/Día5/01-callback.js
@@ -1,3 +1,4 @@
+/*
 //ciudadano de primera clase
 //podemos asignar una función a una variable
 const suma = (a, b) => {
@@ -8,9 +9,9 @@ const suma = (a, b) => {
 const operacion = (funcion, a, b) => {
   return funcion(a, b);
 };
-
+//tanto suma...
 console.log(operacion(suma, 10, 20));
-
+//...como la función flecha anónima que estamos pasando a operación serian callbacks
 console.log(
   operacion(
     (a, b) => {
@@ -20,3 +21,27 @@ console.log(
     20
   )
 );
+*/
+
+//síncrono, todo se va a ejecutar 01 detrás de otro
+//JS va a tener 01 solo hilo de ejecución
+
+//setTimeout va a recibir una función y un tiempo en ms
+//ejecutará la funcion después de ese tiempo
+
+const ejecutarEnOrden = (parametroCallback) => {
+  //tarea mi asincrona, es el setTimeout
+  setTimeout(() => {
+    console.log("Hola!");
+    parametroCallback();
+  }, 3000);
+};
+
+const funcionCallback = () => {
+  for (let i = 0; i < 4; i++) {
+    console.log(i);
+  }
+  console.log("adios");
+}
+
+ejecutarEnOrden(funcionCallback);

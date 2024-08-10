2de5dc63083399be2d0078a35d378ecff51fd9ec
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-09 19:27:18 -0500

    avance

--- Semana6/Día5/01-callback.js
+++ Semana6/Día5/01-callback.js
@@ -1,12 +1,22 @@
 //ciudadano de primera clase
 //podemos asignar una función a una variable
 const suma = (a, b) => {
-    return a + b;
-}
+  return a + b;
+};
 
 //utilizar una función como argumento
 const operacion = (funcion, a, b) => {
-    return funcion(a, b);
-}
+  return funcion(a, b);
+};
 
 console.log(operacion(suma, 10, 20));
+
+console.log(
+  operacion(
+    (a, b) => {
+      return a * b;
+    },
+    10,
+    20
+  )
+);

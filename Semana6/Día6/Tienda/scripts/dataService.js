886fd1c1ef1ccb71bb80dd84f302e2f893cf5b0c
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-10 12:13:41 -0500

    avance

---
+++ Semana6/Día6/Tienda/scripts/dataService.js
@@ -0,0 +1,16 @@
+const URL = "https://66b797cd7f7b1c6d8f1c4b3d.mockapi.io/";
+
+const getProducts = async () => {
+  try {
+    const response = await fetch(`${URL}productos`);
+    if (response.status === 200){
+        const data = await response.json();
+        return data;
+    } else{
+        throw new Error("Error al obtener los datos");
+    }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}

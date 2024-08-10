b259abb2a997a9de3b613dec518e87a47e33abff
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-09 22:42:24 -0500

    avance

--- Semana6/Día5/03-asyncawait.js
+++ Semana6/Día5/03-asyncawait.js
@@ -1,10 +1,20 @@
+/*
 const ejecutar = () => {
   return new Promise((resolve, reject) => {
     resolve("Éxito 😺");
   });
 };
+*/
+// async function ejecutar (){
+const ejecutar = async () => {
+  return "Éxito 😺"; //resolve
+  // throw "Algo falló! 😷"; //reject
+}
 
 ejecutar()
 .then((resultado) => {
   console.log(resultado);
+})
+.catch((error) => {
+  console.log(error)
 })

dcfa66d3ae9279a8b65525cdbd45f45a100e3ac1
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-13 21:43:08 -0500

    avance

---
+++ Semana7/Día2/03-hooks/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  let contador = 0;
+
+  return (
+    <div>
+      <span>Contador: {contador}</span>
+      <br/>
+      <button>Incrementar contador</button>
+    </div>
+  )
+}
+
+export default App

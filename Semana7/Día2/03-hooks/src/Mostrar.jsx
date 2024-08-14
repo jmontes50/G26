d30633fa9b3d494d8498fec1fcc649f8adff491b
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-13 22:55:52 -0500

    avance

--- Semana7/Día2/03-hooks/src/Mostrar.jsx
+++ Semana7/Día2/03-hooks/src/Mostrar.jsx
@@ -1,9 +1,14 @@
 //Un componente también se volverá a dibujar/renderizar cuando reciba nuevos props
 const Mostrar = (props) => {
-    // console.log("props de Mostrar:", props);
-    return (
-        <div>Mostrar contador: {props.contador}</div>
-    )
-}
+  // console.log("props de Mostrar:", props);
+  const porUnMillon = props.contador * 1000000;
 
-export default Mostrar
+  return (
+    <>
+      <div>Mostrar contador: {props.contador}</div>
+      <div>Por un millón: {porUnMillon}</div>
+    </>
+  );
+};
+
+export default Mostrar;

115c91ccfe3ac4b18e6e2349689a15c63a9b9480
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-13 21:58:20 -0500

    avance

--- Semana7/Día2/03-hooks/src/App.jsx
+++ Semana7/Día2/03-hooks/src/App.jsx
@@ -1,11 +1,25 @@
-const App = () => {
-  let contador = 0;
+import { useState } from "react";
 
+const App = () => {
+  //const [estado, setFuncionEstado] = useState(valor_inicial);
+  const [contador, setContador] = useState(0);
+
+  // let contador = 0;
+  const manejarClick = () => {
+    // contador++;
+    // console.log("click contador:", contador);
+    // Cuanco deseemos cambiar el estado invocamos esa función para actualizar el estado 
+    setContador(contador + 1);
+  }
+  //cuando cambie el estado se va a volver a dibujar mi componente
   return (
     <div>
       <span>Contador: {contador}</span>
       <br/>
-      <button>Incrementar contador</button>
+      {/*onClick, onSubmit, onChange, prefijo 'on' después el Evento*/}
+      <button onClick={manejarClick}>
+        Incrementar contador
+        </button>
     </div>
   )
 }

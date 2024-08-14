4514d5999cb09affd78d2c61db00388b3ff613f9
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-13 22:12:31 -0500

    avance

--- Semana7/Día2/03-hooks/src/App.jsx
+++ Semana7/Día2/03-hooks/src/App.jsx
@@ -11,15 +11,23 @@ const App = () => {
     // Cuanco deseemos cambiar el estado invocamos esa función para actualizar el estado 
     setContador(contador + 1);
   }
-  //cuando cambie el estado se va a volver a dibujar mi componente
+
+  const manejarHipervinculo = (ev) => {
+    ev.preventDefault();
+  }
+  //cuando cambie el estado se va a volver a dibujar/renderizar mi componente
   return (
     <div>
       <span>Contador: {contador}</span>
       <br/>
       {/*onClick, onSubmit, onChange, prefijo 'on' después el Evento*/}
+      <a href="https://google.com" onClick={manejarHipervinculo}>
+        Ir a google
+      </a>
+      <br/>
       <button onClick={manejarClick}>
         Incrementar contador
-        </button>
+      </button>
     </div>
   )
 }

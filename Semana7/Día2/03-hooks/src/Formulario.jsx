c52ee490df02516b41044231373f78e7514b9232
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-14 19:27:41 -0500

    avance

--- Semana7/Día2/03-hooks/src/Formulario.jsx
+++ Semana7/Día2/03-hooks/src/Formulario.jsx
@@ -1,7 +1,18 @@
 const Formulario = () => {
   return (
-    <div>Formulario</div>
-  )
-}
+    <>
+      <h2>Formulario</h2>
+      <form>
+        <label htmlFor="nombre">Nombre:</label>
+        <input 
+          id="nombre" 
+          type="text" 
+          placeholder="Ingrese su nombre"
+        />
+        <button type="submit">Guardar</button>
+      </form>
+    </>
+  );
+};
 
-export default Formulario
+export default Formulario;

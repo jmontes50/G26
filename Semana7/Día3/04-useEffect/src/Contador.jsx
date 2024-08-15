65d2f7e3385339a10b2a3a8a12d10834c6f88670
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-14 20:59:10 -0500

    avance

--- Semana7/Día3/04-useEffect/src/Contador.jsx
+++ Semana7/Día3/04-useEffect/src/Contador.jsx
@@ -1,8 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Contador = () => {
   //const [estado, funcActEseEstado] = useState(valor_inicial);
   const [contador, setContador] = useState(0);
+
+  //el useEffect suele estar justo antes del return
+  //useEffect(función, [dependencias])
+  //el useEffect por defecto siempre se va a ejecutar cuando cualquier estado cambie
+  useEffect(() => {
+    console.log(`El contador es: ${contador}`);
+  })
   return (
     <>
       <h2>Contador es: {contador}</h2>

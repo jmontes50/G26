64af8784f5b7a8bb5bf878d405f8c5a489cc4b77
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-14 22:44:53 -0500

    avance

--- Semana7/Día3/05-todo/src/App.jsx
+++ Semana7/Día3/05-todo/src/App.jsx
@@ -1,6 +1,15 @@
 import { useState, useEffect } from "react";
 
 const App = () => {
+  //lista tareas que tengo pendientes
+  const [tareas, setTareas] = useState([]);
+  //componentes controlados, para el input de tarea
+  const [inputTarea, setInputTarea] = useState('');
+
+  const manejarInputTarea = (e) => {
+    setInputTarea(e.target.value);
+  }
+
   return (
     <>
       <div>
@@ -9,6 +18,8 @@ const App = () => {
           type="text"
           id="nueva-tarea"
           placeholder="Ej. Comprar Adobo"
+          value={inputTarea}
+          onChange={manejarInputTarea}
         />
         <br />
         <button>Agregar Tarea</button>

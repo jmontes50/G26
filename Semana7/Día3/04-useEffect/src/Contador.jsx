58028df1bd3b105bc96ae2f5916fecd7ca1d7556
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-14 20:49:39 -0500

    avance

---
+++ Semana7/Día3/04-useEffect/src/Contador.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+const Contador = () => {
+  const [contador, setContador] = useState(0);
+  return (
+    <>
+      <h2>Contador es: {contador}</h2>
+      <button
+        onClick={() => {
+          setContador(contador + 1);
+        }}
+      >
+        Incrementar
+      </button>
+    </>
+  );
+};
+
+export default Contador;

50b2cd0a2c822769454a16e2c8ff91aa4ba0d727
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-15 22:45:19 -0500

    avance

---
+++ Semana7/Día4/07-router/src/components/Detalle.jsx
@@ -0,0 +1,14 @@
+const Detalle = () => {
+  return (
+    <div>
+      <h2>
+        <code>
+          Detalle
+        </code>
+      </h2>
+      <p>Contenido del detalle</p>
+    </div>
+  )
+}
+
+export default Detalle

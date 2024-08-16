5ef5baf884cfca286ef6c4e882126a018ab93cdc
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-15 22:37:43 -0500

    avance

---
+++ Semana7/Día4/07-router/src/components/Inicio.jsx
@@ -0,0 +1,10 @@
+const Inicio = () => {
+  return (
+    <div>
+      <h2>Inicio</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora expedita animi quaerat, at suscipit repellat eaque officia eos laborum debitis sapiente, in placeat nemo fuga ipsum et error. Dolor, ad!</p>
+    </div>
+  )
+}
+
+export default Inicio

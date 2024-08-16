d740e66bbe6643d6829bd8456c85a62097db8185
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-15 21:55:58 -0500

    avance

--- Semana7/Día4/06-peticiones/src/App.jsx
+++ Semana7/Día4/06-peticiones/src/App.jsx
@@ -4,7 +4,9 @@ const App = () => {
   const [productos, setProductos] = useState([]);
   const URL = "https://66b797cd7f7b1c6d8f1c4b3d.mockapi.io/productos";
 
+  //Podemos utilizar async/await en un useEffect pero no podemos ponerlo directamente en la función callback que recibe useEffect---> ej: useEffect(async () => .... ✖️)
   useEffect(() => {
+    /*
     fetch(URL)
     .then((response) => {
       console.log("response", response); //Guía de remisión
@@ -17,6 +19,18 @@ const App = () => {
     .catch((err) => {
       console.log(err);
     });
+    */
+   //si queremos usar async await tenemos que crear una función aparte dentro del useEffect
+    const obtenerDatos = async () => {
+      try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setProductos(data)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    obtenerDatos();
   }, []);
   return (
     <div>

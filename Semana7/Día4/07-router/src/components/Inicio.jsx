31bebe809a77446c42cd70c5402ba90cf3b6c472
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-15 22:53:37 -0500

    avance

--- Semana7/Día4/07-router/src/components/Inicio.jsx
+++ Semana7/Día4/07-router/src/components/Inicio.jsx
@@ -1,8 +1,12 @@
+//Link va a funcionar como una especie de hipervínculo, pero en reemplazo del href tendrá un to
+import { Link } from "react-router-dom"
+
 const Inicio = () => {
   return (
     <div>
       <h2>Inicio</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora expedita animi quaerat, at suscipit repellat eaque officia eos laborum debitis sapiente, in placeat nemo fuga ipsum et error. Dolor, ad!</p>
+      <Link to="/lista">Ir a Lista</Link>
     </div>
   )
 }

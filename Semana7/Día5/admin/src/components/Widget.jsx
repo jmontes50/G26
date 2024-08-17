7b84af766402b7c6cd76ff393d93c6e1db26dcf0
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-17 10:57:52 -0500

    avance

--- Semana7/Día5/admin/src/components/Widget.jsx
+++ Semana7/Día5/admin/src/components/Widget.jsx
@@ -1,14 +1,19 @@
+//que estos estilos lo podemos utilizar aquí y NO van afectar al resto de componentes. A comparacion de cuando importamos directamente un archivo común de css
+import estilos from "./styles/Widget.module.css";
+
 const Widget = (props) => {
   const { color, icono, texto, numero } = props;
 
+  // console.log(estilos)
+
   return (
     <div
-      className="d-flex flex-column border rounded p-3"
+      className={`d-flex flex-column border rounded p-3 gap-3 ${estilos["card-widget"]}`}
       style={{ backgroundColor: color }}
     >
-      <i className={icono}></i>
-      <span className="">{texto}</span>
-      <span className="">{numero}</span>
+      <i className={`${icono} fa-4x mt-2`}></i>
+      <span className={estilos["texto-widget"]}>{texto}</span>
+      <span className={estilos["numero-widget"]}>{numero}</span>
     </div>
   );
 };

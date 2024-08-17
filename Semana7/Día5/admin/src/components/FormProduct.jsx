ca4c8b22f44f6ae129a66332558880fcc7766ce8
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-17 12:47:53 -0500

    avance

--- Semana7/Día5/admin/src/components/FormProduct.jsx
+++ Semana7/Día5/admin/src/components/FormProduct.jsx
@@ -11,23 +11,30 @@ const FormProduct = (props) => {
     cantidad,
     estrellas,
     categoria,
+    //precio-oferta
   } = producto;
-  /**
-   * nombre:'',
-    descripcion:'',
-    precio:0,
-    imagen:'',
-    detalles:'',
-    'precio-oferta':0,
-    cantidad:0,
-    estrellas:0,
-    categoria:''
-   */
+
   return (
     <form className="row">
       <div className="col-12 col-md-6 mb-3">
         <label htmlFor="nombre">Nombre</label>
-        <input type="text" id="nombre" name="nombre" value={producto.nombre} />
+        <input
+          type="text"
+          id="nombre"
+          name="nombre"
+          value={nombre}
+          onChange={manejarInputs}
+        />
+      </div>
+      <div className="col-12 col-md-6 mb-3">
+        <label htmlFor="cantidad">Cantidad</label>
+        <input
+          type="number"
+          id="cantidad"
+          name="cantidad"
+          value={cantidad}
+          onChange={manejarInputs}
+        />
       </div>
     </form>
   );

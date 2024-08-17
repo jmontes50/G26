af6ed25305bbe83c57e53e1c8352357ad9c4618e
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-17 12:20:22 -0500

    avance

--- Semana7/Día5/admin/src/views/CreateProductView.jsx
+++ Semana7/Día5/admin/src/views/CreateProductView.jsx
@@ -1,4 +1,18 @@
+import { useState } from "react";
+
 const CreateProductView = () => {
+  const [nuevoProducto, setNuevoProducto] = useState({
+    nombre:'',
+    descripcion:'',
+    precio:0,
+    imagen:'',
+    detalles:'',
+    'precio-oferta':0,
+    cantidad:0,
+    estrellas:0,
+    categoria:''
+  })
+
   return (
     <div>CreateProductView</div>
   )

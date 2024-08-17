dac80581cd100fec9ffca7968c41a833e4a63dda
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-16 22:51:34 -0500

    avance

--- Semana7/Día5/admin/src/views/HomeView.jsx
+++ Semana7/Día5/admin/src/views/HomeView.jsx
@@ -1,9 +1,12 @@
 //vistas utilizaran los servicios para hacer las operaciones
 import { useState, useEffect } from 'react';
 import { obtenerProductos } from '../services/productService';
+import TableData from '../components/TableData';
 
 const HomeView = () => {
   const [productos, setProductos] = useState([]);
+
+  const cabeceras = ["nombre", "categoria", "cantidad", "precio", "precio-oferta"];
 
   useEffect(() => {
     const getProductos = async () => {

05dbea52d0f4a1b573e3434d8fbdc64de8f8e32b
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-17 09:54:49 -0500

    avance

--- Semana7/Día5/admin/src/main.jsx
+++ Semana7/Día5/admin/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+//Si uds importan un archivo de css en cualquier parte de react este podrá ser utilizado en todas partes
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(

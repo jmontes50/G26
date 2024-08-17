cfe078d9e91ceb77f00cd06e961efde20759da41
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-17 10:38:36 -0500

    avance

--- Semana7/Día5/admin/src/components/Widget.jsx
+++ Semana7/Día5/admin/src/components/Widget.jsx
@@ -2,12 +2,15 @@ const Widget = (props) => {
   const { color, icono, texto, numero } = props;
 
   return (
-    <div className="d-flex flex-column border rounded p-3" style={{color:color}}>
+    <div
+      className="d-flex flex-column border rounded p-3"
+      style={{ backgroundColor: color }}
+    >
       <i className={icono}></i>
       <span className="">{texto}</span>
       <span className="">{numero}</span>
     </div>
-  )
-}
+  );
+};
 
-export default Widget
+export default Widget;

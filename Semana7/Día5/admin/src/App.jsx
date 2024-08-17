d458c72693ebe52bac044f50fa8046ac8dccc187
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-17 11:56:55 -0500

    avance

--- Semana7/Día5/admin/src/App.jsx
+++ Semana7/Día5/admin/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
 import HomeView from "./views/HomeView";
+import CreateProductView from "./views/CreateProductView";
 //import "./assets/tuCSS.css"
 
 const App = () => {
@@ -8,11 +10,14 @@ const App = () => {
       <div className="row">
         <div className="col-3">
           {/* Navegación */}
+          <Navigation />
+          {/* para que cualquier componente Link de react-router-dom funcione, tiene que estar dentro del browser router */}
         </div>
         <div className="col-9 pt-3">
           {/* Rutas van a ir aquí, para que se dibujen solo en esta parte */}
           <Routes>
             <Route path="/" element={<HomeView />} />
+            <Route path="/createproduct" element={<CreateProductView />} />
           </Routes>
         </div>
       </div>

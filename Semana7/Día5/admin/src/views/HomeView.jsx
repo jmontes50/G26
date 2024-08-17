8702cd2f7f186bc83fb3e894dd1471348efadba3
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-16 22:19:47 -0500

    avance

--- Semana7/Día5/admin/src/views/HomeView.jsx
+++ Semana7/Día5/admin/src/views/HomeView.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+//vistas utilizaran los servicios para hacer las operaciones
+import { useState, useEffect } from 'react';
+import { obtenerProductos } from '../services/productService';
 
 const HomeView = () => {
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    const getProductos = async () => {
+      try {
+        const productosObtenidos = await obtenerProductos();
+        setProductos(productosObtenidos);
+      } catch (error) {
+        alert("Ocurrio un error");
+        console.log(error);
+      }
+    }
+    getProductos();
+  }, []);
+
   return (
     <div>HomeView</div>
   )

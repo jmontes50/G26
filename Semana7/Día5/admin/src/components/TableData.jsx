5da92c74ee36efe56dc067dc0c2e4ae0df34e049
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-16 23:03:48 -0500

    avance

--- Semana7/Día5/admin/src/components/TableData.jsx
+++ Semana7/Día5/admin/src/components/TableData.jsx
@@ -14,6 +14,17 @@ const TableData = (props) => {
           ))}
         </tr>
       </thead>
+      <tbody>
+        {/* recorrer datos... datos productos y cada producto tiene nombre, id, precio, descripcion */}
+        {datos.map((fila, i) => (
+          <tr key={i}>
+            {/* recorrer por cada producto cada campo que indiquemos a partir de las cabeceras */}
+            {cabeceras.map((cab, j) => (
+              <td key={j}>{fila[cab]}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
     </table>
   )
 }

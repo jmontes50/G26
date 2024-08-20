959f6d4d82491f1c9ca594f2c3d0edb819f85542
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-19 19:28:31 -0500

    avance

--- Semana7/Día5/admin/src/components/FormProduct.jsx
+++ Semana7/Día5/admin/src/components/FormProduct.jsx
@@ -20,7 +20,9 @@ const FormProduct = (props) => {
       {/* cada input, text-area tiene que tener la clase form-control */}
       {/* nombre */}
       <div className="col-12 col-md-6 mb-3">
-        <label className="form-label" htmlFor="nombre">Nombre</label>
+        <label className="form-label" htmlFor="nombre">
+          Nombre
+        </label>
         <input
           className="form-control"
           type="text"
@@ -32,7 +34,9 @@ const FormProduct = (props) => {
       </div>
       {/* cantidad */}
       <div className="col-12 col-md-6 mb-3">
-        <label className="form-label" htmlFor="cantidad">Cantidad</label>
+        <label className="form-label" htmlFor="cantidad">
+          Cantidad
+        </label>
         <input
           className="form-control"
           type="number"
@@ -44,7 +48,9 @@ const FormProduct = (props) => {
       </div>
       {/* precio */}
       <div className="col-12 col-md-6 mb-3">
-        <label className="form-label" htmlFor="precio">Precio</label>
+        <label className="form-label" htmlFor="precio">
+          Precio
+        </label>
         <input
           className="form-control"
           type="number"
@@ -56,21 +62,45 @@ const FormProduct = (props) => {
       </div>
       {/* precio-oferta */}
       <div className="col-12 col-md-6 mb-3">
-        <label className="form-label" htmlFor="precio-oferta">Precio oferta</label>
+        <label className="form-label" htmlFor="precio-oferta">
+          Precio oferta
+        </label>
         <input
           className="form-control"
           type="number"
           id="precio-oferta"
           name="precio-oferta"
           // considerar que aqui el nombre de la propiedad es 'precio-oferta'
-          value={producto['precio-oferta']}
+          value={producto["precio-oferta"]}
           onChange={manejarInputs}
         />
       </div>
       {/* descripción */}
-
+      <div className="col-12 mb-3">
+        <label className="form-label" htmlFor="descripcion">
+          Descripción
+        </label>
+        <textarea
+          className="form-control"
+          id="descripcion"
+          name="descripcion"
+          value={descripcion}
+          onChange={manejarInputs}
+        />
+      </div>
       {/* detalles */}
-
+      <div className="col-12 mb-3">
+        <label className="form-label" htmlFor="detalles">
+          Detalles
+        </label>
+        <textarea
+          className="form-control"
+          id="detalles"
+          name="detalles"
+          value={detalles}
+          onChange={manejarInputs}
+        />
+      </div>
     </form>
   );
 };

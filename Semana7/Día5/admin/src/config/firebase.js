9237fc8a80f6e861fd07e90a5011b6cf37baf5d7
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-19 22:29:01 -0500

    avance

---
+++ Semana7/Día5/admin/src/config/firebase.js
@@ -0,0 +1,13 @@
+//Una referencia a nuestro propia aplicación ya registrada de firebase
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    //import.meta.env.VITE_
+    apiKey: import.meta.env.VITE_API_KEY,  
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,  
+    projectId: import.meta.env.VITE_PROJECT_ID,  
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,  
+    messagingSenderId: import.meta.env.VITE_SENDER_ID,  
+    appId: import.meta.env.VITE_APP_ID 
+  };
+  

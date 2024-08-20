c53d97e6f6d7bb0afcb19b7b1c0a082a8c1dc50b
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-19 22:30:46 -0500

    avance

--- Semana7/Día5/admin/src/config/firebase.js
+++ Semana7/Día5/admin/src/config/firebase.js
@@ -1,13 +1,16 @@
 //Una referencia a nuestro propia aplicación ya registrada de firebase
 import { initializeApp } from "firebase/app";
 
+//firebase nos da unas credenciales asi que las estamos trayendo de las variables de entorno configuradas previamente
 const firebaseConfig = {
-    //import.meta.env.VITE_
+//import.meta.env.VITE_
     apiKey: import.meta.env.VITE_API_KEY,  
     authDomain: import.meta.env.VITE_AUTH_DOMAIN,  
     projectId: import.meta.env.VITE_PROJECT_ID,  
     storageBucket: import.meta.env.VITE_STORAGE_BUCKET,  
     messagingSenderId: import.meta.env.VITE_SENDER_ID,  
     appId: import.meta.env.VITE_APP_ID 
-  };
-  
+};
+
+//tenemos una referencia a la aplicación registrada
+const app = initializeApp(firebaseConfig);

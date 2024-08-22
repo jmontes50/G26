b174763db4ee9c9fcaf9661354b1d7ad6fbf20d6
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-21 20:31:54 -0500

    avance

--- Semana7/Día5/admin/src/components/Loading.jsx
+++ Semana7/Día5/admin/src/components/Loading.jsx
@@ -2,9 +2,9 @@ import estilos from "./styles/loading.module.css";
 
 const Loading = () => {
   return (
-    <div class={estilos["loading-container"]}>
+    <div className={estilos["loading-container"]}>
       <i
-        class={`fa-solid fa-spinner fa-spin-pulse fa-4x text-primary ${estilos["loading-icon"]}`}
+        className={`fa-solid fa-spinner fa-spin-pulse fa-4x text-primary ${estilos["loading-icon"]}`}
       ></i>
     </div>
   );
--- Semana7/Día5/admin/src/views/EditProductView.jsx
+++ Semana7/Día5/admin/src/views/EditProductView.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { obtenerProductoPorId } from "../services/productService";
+import Loading from "../components/Loading";
 
 const EditProductView = () => {
   const [producto, setProducto] = useState(null);
+  const [cargando, setCargando] = useState(false);
 
   const { id } = useParams();
   // console.log("id editproductview", id);
@@ -11,17 +13,23 @@ const EditProductView = () => {
   useEffect(() => {
     const getProduct = async () => {
       try {
+        setCargando(true);
         const productoObtenido = await obtenerProductoPorId(id);
-        console.log("producto obtenido: ", productoObtenido)
+        setProducto(productoObtenido);
+        setCargando(false);
       } catch (error) {
         alert("Ocurrio un error intente de nuevo");
+        setCargando(false);
       }
     }
     getProduct();
   }, []);
 
   return (
-    <div>EditProductView</div>
+    <div>
+      <h2 className="mb-4">Editar Producto</h2>
+      {cargando ? <Loading /> : null}
+    </div>
   )
 }
 

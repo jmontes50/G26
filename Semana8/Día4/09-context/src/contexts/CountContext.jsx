5dfac77099236cacfec79b14fb7665a570fb031d
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-22 21:48:22 -0500

    avance

---
+++ Semana8/Día4/09-context/src/contexts/CountContext.jsx
@@ -0,0 +1,19 @@
+import { useState, createContext } from "react";
+
+//creando el contexto y dentro de ese contexto estará el provider, pero esta referencia va a ser vital para que cuando se obtenga los datos se sepa a quien nos estamos refiriendo 
+const CountContext = createContext();
+
+//este va a ser un componente por el que se compartira lo que deseemos
+const CountContextProvider = ({ children }) => {
+  const [contador, setContador] = useState(0);
+
+  //lo que deseemos compartir ira en un prop llamado value
+  return <CountContext.Provider value={{ contador: contador }}>
+    { children }
+  </CountContext.Provider>
+}
+
+export {
+  CountContext,
+  CountContextProvider
+}

0054acba9ade371ac8c6309f4b00c5df94ed4bb4
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-22 22:13:03 -0500

    avance

---
+++ Semana8/Día4/09-context/src/components/CambiarContador.jsx
@@ -0,0 +1,10 @@
+import { useContext } from 'react';
+import { CountContext } from '../contexts/CountContext';
+
+const CambiarContador = () => {
+  return (
+    <div>CambiarContador</div>
+  )
+}
+
+export default CambiarContador

a296d95483528b61355296182570759f1b0565cf
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-22 22:07:34 -0500

    avance

--- Semana8/Día4/09-context/src/components/MostrarContador.jsx
+++ Semana8/Día4/09-context/src/components/MostrarContador.jsx
@@ -1,14 +1,17 @@
 //useContext para poder 'consumir' el contexto
-import { useContext } from 'react';
+import { useContext } from "react";
 //y la referencia del contexto porque podemos tener varios
-import { CountContext } from '../contexts/CountContext';
+import { CountContext } from "../contexts/CountContext";
 
 const MostrarContador = () => {
-  const datos = useContext(CountContext);
-  console.log(datos);
+  const { contador } = useContext(CountContext);
+  // console.log(datos);
   return (
-    <div>MostrarContador</div>
-  )
-}
+    <div>
+      <h3>Mostrar Contador</h3>
+      <p>El contador es: {contador}</p>
+    </div>
+  );
+};
 
-export default MostrarContador
+export default MostrarContador;

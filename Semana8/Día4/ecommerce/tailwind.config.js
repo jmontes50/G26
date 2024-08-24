58af920a0b65e3427661af64778256dab80b191a
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-24 09:54:18 -0500

    avance

--- Semana8/Día4/ecommerce/tailwind.config.js
+++ Semana8/Día4/ecommerce/tailwind.config.js
@@ -3,7 +3,20 @@ export default {
   // deseo que tengas en cuenta la carpeta src, con cualquier carpeta adicional y cualquier archivo con esas extensiones
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    // modificar la configuración y valores que maneja tailwind
+    extend: {
+      fontFamily: {
+        // la fuente predeterminada de Tailwind es Sans
+        sans: ["Jost", "sans-serif"],
+        jost: ["Jost", "sans-serif"]
+      },
+      fontWeight: {
+        light: 300,
+        normal: 400,
+        medium: 500,
+        bold: 700
+      }
+    },
   },
   plugins: [],
 }

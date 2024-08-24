dddf6f229f2fd9cc1ef6b6d6e95c8e11b281bde0
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-24 10:55:35 -0500

    avance

--- Semana8/Día4/ecommerce/src/App.jsx
+++ Semana8/Día4/ecommerce/src/App.jsx
@@ -3,6 +3,8 @@ import { AuthContextProvider } from "./contexts/AuthContext";
 import { ThemeContextProvider } from "./contexts/ThemeContext";
 import HomeView from "./views/HomeView";
 import LoginView from "./views/LoginView";
+// components
+import Navbar from "./components/ui/Navbar";
 
 const App = () => {
   return (
@@ -10,6 +12,7 @@ const App = () => {
       <AuthContextProvider>
         <ThemeContextProvider>
           <div className="dark">
+            <Navbar />
             <h1 className="">Ecommerce</h1>
             <Link to="/login">Ir a Login</Link>
             <Routes>

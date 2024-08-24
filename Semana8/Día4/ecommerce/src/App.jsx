ddc8c0f9739068b718afbae8d86cc443e3cb8d85
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-24 11:04:43 -0500

    avance

--- Semana8/Día4/ecommerce/src/App.jsx
+++ Semana8/Día4/ecommerce/src/App.jsx
@@ -1,19 +1,25 @@
+import { useContext } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { AuthContextProvider } from "./contexts/AuthContext";
 import { ThemeContextProvider } from "./contexts/ThemeContext";
+import { ThemeContext } from "./contexts/ThemeContext";
 import HomeView from "./views/HomeView";
 import LoginView from "./views/LoginView";
 // components
 import Navbar from "./components/ui/Navbar";
 
 const App = () => {
+
+  const { isDark } = useContext(ThemeContext);
+  console.log(isDark)
+
   return (
     <BrowserRouter>
       <AuthContextProvider>
         <ThemeContextProvider>
-          <div className="dark">
+          <div className={ isDark ? 'dark' : '' }>
             <Navbar />
-            <h1 className="">Ecommerce</h1>
+            <h1 className="dark:bg-dark-background text-light-text">Ecommerce</h1>
             <Link to="/login">Ir a Login</Link>
             <Routes>
               <Route path="/" element={<HomeView />} />

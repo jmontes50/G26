7ef96c899842c225bcb6c1608adf943650ad73ef
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-23 22:52:14 -0500

    avance

---
+++ Semana8/Día4/ecommerce/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // deseo que tengas en cuenta la carpeta src, con cualquier carpeta adicional y cualquier archivo con esas extensiones
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

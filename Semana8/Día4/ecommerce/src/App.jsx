d09c3ed869b016d58b65ccb69f52973c3ce1b988
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-23 20:49:53 -0500

    avanc

--- Semana8/Día4/ecommerce/src/App.jsx
+++ Semana8/Día4/ecommerce/src/App.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthContextProvider } from "./contexts/AuthContext";
 import HomeView from "./views/HomeView";
 import LoginView from "./views/LoginView";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <h1>Ecommerce</h1>
-      <Routes>
-        <Route path="/" element={<HomeView />} />
-        <Route path="/login" element={<LoginView />} />
-      </Routes>
+      <AuthContextProvider>
+        <h1>Ecommerce</h1>
+        <Routes>
+          <Route path="/" element={<HomeView />} />
+          <Route path="/login" element={<LoginView />} />
+        </Routes>
+      </AuthContextProvider>
     </BrowserRouter>
   )
 }

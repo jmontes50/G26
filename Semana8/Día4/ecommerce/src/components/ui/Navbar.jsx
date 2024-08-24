433f658c30d4134f9f7a15921f168484d1df845b
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-24 12:27:10 -0500

    avance

--- Semana8/Día4/ecommerce/src/components/ui/Navbar.jsx
+++ Semana8/Día4/ecommerce/src/components/ui/Navbar.jsx
@@ -8,6 +8,20 @@ const Navbar = () => {
     <nav className="dark:bg-dark-background dark:text-dark-text">
       {/* container */}
       <div className="px-4 mx-auto lg:px-8 xl:max-w-7xl">
+        <div className="flex justify-between py-4">
+          {/* logo */}
+          <div className="flex items-center">
+            Logo
+          </div>
+          {/* ul */}
+          <ul className="flex items-center justify-center gap-10">
+            <li>Inicio</li>
+            <li>Productos</li>
+            <li>Carrito</li>
+          </ul>
+          {/* div con otros items */}
+          
+        </div>
         <button className="btn btn-primary" onClick={toggleDarkMode}>
           Cambiar Tema {isDark ? "claro" : "oscuro"}
         </button>

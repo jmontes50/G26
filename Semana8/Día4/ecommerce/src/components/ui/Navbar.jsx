66cbea7ad7baa0ada5e9a17b748e8c1c60fc43cc
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-24 12:17:41 -0500

    avance

--- Semana8/Día4/ecommerce/src/components/ui/Navbar.jsx
+++ Semana8/Día4/ecommerce/src/components/ui/Navbar.jsx
@@ -5,11 +5,14 @@ const Navbar = () => {
   const { isDark, toggleDarkMode } = useContext(ThemeContext);
 
   return (
-    <div>
-      <button className="btn btn-primary" onClick={toggleDarkMode}>
-        Cambiar Tema {isDark ? "claro" : "oscuro"}
-      </button>
-    </div>
+    <nav className="dark:bg-dark-background dark:text-dark-text">
+      {/* container */}
+      <div className="px-4 mx-auto lg:px-8 xl:max-w-7xl">
+        <button className="btn btn-primary" onClick={toggleDarkMode}>
+          Cambiar Tema {isDark ? "claro" : "oscuro"}
+        </button>
+      </div>
+    </nav>
   );
 };
 

d9e1e075184762e3d95babf7925bddd8106bd8c8
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-23 20:40:42 -0500

    avanc

--- Semana8/Día4/ecommerce/src/contexts/AuthContext.jsx
+++ Semana8/Día4/ecommerce/src/contexts/AuthContext.jsx
@@ -1,11 +1,20 @@
 //para que toda la información y manejo del login este aquí
 import { createContext, useState, useEffect } from "react";
-import { GoogleAuthProvider } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../config/firebase";
 
 const googleProvider = new GoogleAuthProvider();
 
 const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
-  
+  const [user, setUser] = useState(null);
+
+  const loginWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+    } catch (error) {
+      console.log(error)
+    }
+  }
 }

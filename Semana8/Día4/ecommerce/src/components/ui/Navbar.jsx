e2cf9ab2602b7cf1ca72ba26756348137d8a940a
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-24 12:00:34 -0500

    avance

--- Semana8/Día4/ecommerce/src/components/ui/Navbar.jsx
+++ Semana8/Día4/ecommerce/src/components/ui/Navbar.jsx
@@ -1,16 +1,16 @@
-import { useContext } from 'react';
-import { ThemeContext } from '../../contexts/ThemeContext';
+import { useContext } from "react";
+import { ThemeContext } from "../../contexts/ThemeContext";
 
 const Navbar = () => {
   const { isDark, toggleDarkMode } = useContext(ThemeContext);
 
   return (
     <div>
-      <button onClick={toggleDarkMode}>
-        Cambiar Tema { isDark ? "claro": "oscuro" }
+      <button className="btn btn-primary" onClick={toggleDarkMode}>
+        Cambiar Tema {isDark ? "claro" : "oscuro"}
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

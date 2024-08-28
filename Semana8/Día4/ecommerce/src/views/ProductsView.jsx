181a9d71acca37b26c8405f67c2100b10af9f981
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-27 22:09:00 -0500

    avance

--- Semana8/Día4/ecommerce/src/views/ProductsView.jsx
+++ Semana8/Día4/ecommerce/src/views/ProductsView.jsx
@@ -1,4 +1,13 @@
+import useGetAxios from "../hooks/useGetAxios"
+
 const ProductsView = () => {
+  const URL = "https://json-server-vercel-eosin-tau.vercel.app/products"
+  const { data, loading, error } = useGetAxios(URL);
+  
+  console.log("data", data)
+  console.log("loading", loading)
+  console.log("error", error)
+
   return (
     <div>ProductsView</div>
   )

9388fdf8274a78fd8956e463ebf5b969fc8278c8
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-27 22:52:08 -0500

    avance

--- Semana8/Día4/ecommerce/src/components/ui/ProductCard.jsx
+++ Semana8/Día4/ecommerce/src/components/ui/ProductCard.jsx
@@ -1,19 +1,21 @@
 const ProductCard = ({ product }) => {
   const { id, nombre, descripcion, precio, precio_oferta, imagen } = product;
   return (
-    <div className="flex flex-col gap-0.5 max-w-lg">
+    <div className="flex flex-col max-w-lg gap-2">
       {/* contedor imagen */}
-      <div className="object-cover w-100 h-96">
-        <img src={imagen} alt={`foto ${nombre}`} />
+      <div className="object-cover w-full overflow-hidden aspect-auto">
+        <img src={imagen} alt={`foto ${nombre}`} className="w-full"/>
       </div>
-      <h4 className="font-semibold">{nombre}</h4>
+      <h4 className="mt-2 font-semibold">{nombre}</h4>
       <p>{descripcion}</p>
       <div className="flex gap-2">
         <span className="font-semibold">S/ {precio_oferta.toFixed(2)}</span>
-        <span className="text-gray-500 line-through">S/ {precio.toFixed(2)}</span>
+        <span className="text-gray-500 line-through">
+          S/ {precio.toFixed(2)}
+        </span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;

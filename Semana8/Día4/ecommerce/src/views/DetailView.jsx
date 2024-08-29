cc62bd74b83cc77547dee6da49803dd51347f5f0
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-28 19:44:43 -0500

    avance

--- Semana8/Día4/ecommerce/src/views/DetailView.jsx
+++ Semana8/Día4/ecommerce/src/views/DetailView.jsx
@@ -4,13 +4,22 @@ import useGetAxios from "../hooks/useGetAxios";
 const DetailView = () => {
   const { id } = useParams();
   console.log("id params", id);
-  const URL = 'https://json-server-vercel-eosin-tau.vercel.app/products';
-  const { data, loading, error } = useGetAxios(`${URL}/${id}`)
+  const URL = "https://json-server-vercel-eosin-tau.vercel.app/products";
+  const { data, loading, error } = useGetAxios(`${URL}/${id}`);
   console.log("data detailview", data);
-  
-  return (
-    <div>DetailView</div>
-  )
-}
 
-export default DetailView
+  return (
+    <div className="container py-10">
+      {/* <h1 className="mb-10 text-4xl text-center">Productos</h1> */}
+      {data ? (
+        <div className="flex">
+          <div className="w-1/2 aspect-auto md:w-full">
+            <img src={data.imagen} alt={data.nombre} />
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default DetailView;

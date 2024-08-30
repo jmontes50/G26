0d2996a1b60c3f5f140217f04b0f48ffff43eaf9
Osmar Montesinos <osmarmontes50@gmail.com>
2024-08-29 19:47:10 -0500

    avance

--- Semana8/Día4/ecommerce/src/views/CartView.jsx
+++ Semana8/Día4/ecommerce/src/views/CartView.jsx
@@ -1,7 +1,33 @@
 const CartView = () => {
   return (
-    <div>CartView</div>
-  )
-}
+    <div className="container py-10">
+      <h1 className="mb-5 text-4xl font-semibold text-center">Checkout</h1>
+      <div className="flex flex-col w-full gap-5 md:flex-row">
+        {/* contenedor tabla y form envio */}
+        <div className="w-full p-5 md:w-2/3">Tabla</div>
+        {/* subtotal y confirmar compra */}
+        <div className="w-full p-5 md:w-1/3">
+          <div className="flex justify-between mb-5 font-semibold">
+            <span>Subtotal</span>
+            <span>S/ 200</span>
+          </div>
+          {/* descuento */}
+          <div className="mb-5">
+            <span className="text-xs">Ingrese cupón</span>
+            <div className="flex">
+              <input
+                className="p-4 text-lg border-2 border-black rounded-s-lg dark:border-white dark:bg-dark-background"
+                placeholder="FLAT50"
+              />
+              <button className="px-5 text-white bg-slate-950 dark:bg-white dark:text-black rounded-e-lg">
+                Aplicar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CartView
+export default CartView;

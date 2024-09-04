bcc0e0b72dab1b7ed937d4e139ec8651394429db
Osmar Montesinos <osmarmontes50@gmail.com>
2024-09-03 21:59:50 -0500

    avance

--- Semana10/Día2/10-redux/src/app/data/dataSlice.js
+++ Semana10/Día2/10-redux/src/app/data/dataSlice.js
@@ -1,4 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+//createAsyncThunk me pide un nombre y la función asincrona por ejecutar
+const fetchUsers = createAsyncThunk(
+  'users/fetchUsers',
+  async (_ , thunkApi) => {
+    try {
+      //vamos a hacer la petición, la tarea asincrona
+      const result = fetch('https://reqres.in/api/users?page=2');
+      return result.json(); //aca ya esta transformado de JSON a un objeto de JS
+    } catch (error) {
+      thunkApi.rejectWithValue(error);
+    }
+  }
+)
 
 const dataSlice = createSlice({
   name:'users',
